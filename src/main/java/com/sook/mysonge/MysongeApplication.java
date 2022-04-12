654072a41a3e2d68d73de0a4a7235925d5fa710c
yejin <yaezzin@gmail.com>
2022-04-12 12:44:50 +0900

    commit

---
+++ src/main/java/com/sook/mysonge/MysongeApplication.java
@@ -0,0 +1,13 @@
+package com.sook.mysonge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysongeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysongeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sook/mysonge/MysongeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sook.mysonge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysongeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
