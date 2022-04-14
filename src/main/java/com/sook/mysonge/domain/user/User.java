71ff50b9c541daae9e9d706049469d711e983807
Suah Kang <63537847+rkdtndk99@users.noreply.github.com>
2022-04-14 13:20:14 +0900

    [ADD]UserRepository Test 완료

--- src/main/java/com/sook/mysonge/domain/user/User.java
+++ src/main/java/com/sook/mysonge/domain/user/User.java
@@ -1,12 +1,10 @@
 package com.sook.mysonge.domain.user;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Getter
 @NoArgsConstructor
@@ -16,4 +14,20 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String birthday;
+
+    @Builder
+    public User(String name, String email, String birthday){
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+    }
 }
