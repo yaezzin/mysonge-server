4460c3e685e03360a1fc053df51716e5bf5a38cb
yejin <yaezzin@gmail.com>
2022-05-17 01:33:11 +0900

    [ADD] Character Entity

---
+++ src/main/java/com/sook/mysonge/domain/character/Character.java
@@ -0,0 +1,42 @@
+package com.sook.mysonge.domain.character;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Table(name = "Songe")
+@Entity
+public class Character {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @OneToOne(fetch =FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String quote;
+
+    @Builder
+    public Character(String name, String quote) {
+        this.name = name;
+        this.quote = quote;
+    }
+
+    public void update(String name, String quote) {
+        this.name = name;
+        this.quote = quote;
+    }
+
+}
+
+
